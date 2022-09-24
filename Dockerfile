f52d55589dd01aa65f36cd67b3c8053f7a7724d4
Rafael N. Siqueira <rafael.siqueira@sonda.com>
2022-09-24 16:59:43 -0300

    config deployments to kubenetes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
-ARG JAR_FILE=build/libs/accounting-tax-1.0.0.jar
+FROM openjdk:11-jre
+ARG JAR_FILE=build/libs/accounting-tax-1.0.1.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+CMD ["java","-jar","/app.jar"]
--- src/test/java/br/com/rns/acception/BehaviorExecutionTaxTest.java
+++ src/test/java/br/com/rns/acception/BehaviorExecutionTaxTest.java
@@ -3,11 +3,8 @@ package br.com.rns.acception;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Então;
-import io.cucumber.junit.Cucumber;
 import io.cucumber.spring.CucumberContextConfiguration;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
 @CucumberContextConfiguration
 public class BehaviorExecutionTaxTest {
     @Dado("os parâmetros da nota razão social {string}. mês da nota emitida {string}, valor {int}")
