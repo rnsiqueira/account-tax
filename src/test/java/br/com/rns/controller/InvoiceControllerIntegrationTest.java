91cbcc017650dfc62298f4852cd09b2d7c047c74
Rafael Siqueira <rafael.siqueira@contabilizei.com.br>
2022-10-12 22:09:03 -0300

    fix test simple

--- src/test/java/br/com/rns/controller/InvoiceControllerIntegrationTest.java
+++ src/test/java/br/com/rns/controller/InvoiceControllerIntegrationTest.java
@@ -1,14 +1,12 @@
 package br.com.rns.controller;
 
 
-import br.com.rns.ApplicationStart;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = ApplicationStart.class)
+
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class InvoiceControllerIntegrationTest {
